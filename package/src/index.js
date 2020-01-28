88b995b6e68cc235e3511429c78059e494773b8a
Will Wise <william.wise@siteminder.com>
2020-01-28 18:01:55 +1100

    add missing dependency vue-template-compiler and remove duplicate install

--- package/src/index.js
+++ package/src/index.js
@@ -1,10 +1,7 @@
-import VueCompositionApi from '@vue/composition-api'
 import MyButton from './my-button.vue'
 
 const MyPlugin = {
   install: (Vue) => {
-
-    Vue.use(VueCompositionApi)
 
     Vue.component('my-button', MyButton)
 
